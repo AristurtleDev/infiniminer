1b1d7d1fcdb141fcd52a24986f02662b0578e88d
Christopher Whitley <aristurtledev@gmail.com>
2023-10-12 18:50:58 -0400

    Update for nullable context support

--- source/Infiniminer/Infiniminer.Shared/Player.cs
+++ source/Infiniminer/Infiniminer.Shared/Player.cs
@@ -184,10 +184,46 @@ namespace Infiniminer
 
             if (gameInstance != null)
             {
-                this.SpriteModel = new SpriteModel(gameInstance, 4);
-                UpdateSpriteTexture();
+                Texture2D tex = gameInstance.Content.Load<Texture2D>(GenerateTextureName());
+                this.SpriteModel = new SpriteModel(gameInstance, 4, tex);
                 this.IdleAnimation = true;
             }
+        }
+
+        private string GenerateTextureName()
+        {
+            string name = "sprites/tex_sprite_";
+
+            if (team == PlayerTeam.Red)
+            {
+                name += "red_";
+            }
+            else
+            {
+                name += "blue_";
+            }
+
+            switch (tool)
+            {
+                case PlayerTools.ConstructionGun:
+                case PlayerTools.DeconstructionGun:
+                    name += "construction";
+                    break;
+                case PlayerTools.Detonator:
+                    name += "detonator";
+                    break;
+                case PlayerTools.Pickaxe:
+                    name += "pickaxe";
+                    break;
+                case PlayerTools.ProspectingRadar:
+                    name += "radar";
+                    break;
+                default:
+                    name += "pickaxe";
+                    break;
+            }
+
+            return name;
         }
 
         private void UpdateSpriteTexture()
@@ -195,31 +231,9 @@ namespace Infiniminer
             if (gameInstance == null)
                 return;
 
-            string textureName = "sprites/tex_sprite_";
-            if (team == PlayerTeam.Red)
-                textureName += "red_";
-            else
-                textureName += "blue_";
-            switch (tool)
-            {
-                case PlayerTools.ConstructionGun:
-                case PlayerTools.DeconstructionGun:
-                    textureName += "construction";
-                    break;
-                case PlayerTools.Detonator:
-                    textureName += "detonator";
-                    break;
-                case PlayerTools.Pickaxe:
-                    textureName += "pickaxe";
-                    break;
-                case PlayerTools.ProspectingRadar:
-                    textureName += "radar";
-                    break;
-                default:
-                    textureName += "pickaxe";
-                    break;
-            }
-            SpriteModel?.SetSpriteTexture(gameInstance.Content.Load<Texture2D>(textureName));
+            string contentPath = GenerateTextureName();
+            Texture2D texture = gameInstance.Content.Load<Texture2D>(contentPath);
+            SpriteModel?.SetSpriteTexture(texture);
         }
 
         static uint uniqueId = 0;
